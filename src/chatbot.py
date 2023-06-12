649918fb44efcbb3e048b9e18277a4f608d47f95
dirky <catpod1@tcnj.edu>
2023-06-12 10:37:04 -0400

    didnt do much but going to start on my actual work

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,3 +1,18 @@
 from chatterbot import Chatbot
 bot = Chatbot('James')
 
+bot = Chatbot(
+    'James',
+    storage_adapter = 'chatterbot.storage.SQLStorageAdapter',
+    database_uri = 'sqlite:///database.sqlite3',
+    logic_adapters = [
+        'chatterbot.logic.MathematicalEvaluation'
+    ]
+)
+
+while True:
+    try:
+        bot_input = bot.get_response(input())
+        print(bot_input)
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
